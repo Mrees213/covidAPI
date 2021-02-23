5b70ae5e071a92a2514f91af5e7bf18a9b8041ad
Morgan Rees <morgan.w.rees@gmail.com>
2021-02-23 10:26:35 -0600

    Initial Commit

---
+++ API/APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIApp.swift
+//  API
+//
+//  Created by Morgan w Rees on 2/23/21.
+//
+
+import SwiftUI
+
+@main
+struct APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
